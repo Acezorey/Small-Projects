7780c587a52c6a44ed33b90a7f44c97e2468b47b
Ace <130952616+Acezorey@users.noreply.github.com>
2023-07-05 22:29:44 -1000

    Update InputTerminal.java
    Did not show any of the code initially for some reason (im new to this)

---
+++ Java Projects/Calculator/InputTerminal.java
@@ -0,0 +1,42 @@
+/**
+ * @author Ace Reyes
+ * Created on July 4, 2023
+ */
+
+
+package calculator;
+
+import java.util.Scanner;
+
+public class InputTerminal {
+	public static void main(String[] args) {
+		InputProcessor f = new InputProcessor();
+		@SuppressWarnings("resource")
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println(
+		"=======================================================================================" +
+		"\nCalculator version 0.0.1 (wip)" +
+		"\n" +
+		"\nWelcome!" +
+		"\n" + 
+		"\nIn order to use this calculator, input expressions into the terminal" +
+		"\nExample expressions include: 1 + 2, 3 * 12, 5(5 - 2)" +
+		"\n" +
+		"\nThis calculator only recognizes numbers, operators, parenthesis, and 'A'" +
+		"\nRecognized operators: +, -, /, *, ^, %" +
+		"\nThe character 'A' stands for the result of the previous input" +
+		"\n" +
+		"\nIn order to exit, type 'Exit'" +
+		"\n=======================================================================================");
+		
+		while(true) {
+			String input = scan.nextLine();
+			if(input.equals("Exit")) {
+				System.out.println("Exiting calculator...");
+				break;
+			}
+			System.out.println(f.process(input));
+		}
+	}
+}
